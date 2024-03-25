27be23f2b85f273aae718e2aae6185eb9a0c1a92
Sabrina <141427192+AraujoSabrina@users.noreply.github.com>
2024-03-25 16:26:44 -0300

    Inserção do JS
    Funcao PEGARLETRA

---
+++ script.js
@@ -0,0 +1,21 @@
+var num = 9
+if(num < 7){
+    console.log("opa");
+}else{
+    console.log("ola");
+}
+
+var obj = {
+    nome: "Sabrina",
+    idade: 20,
+    desc: "oo"
+}
+console.log(obj.idade)  
+
+function btn_castrar(){
+    console.log('cadstro-btn');
+}
+function pegaLetra(event){
+    console.log(event.key);
+    
+}
